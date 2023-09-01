e44e9257a6ee7f12351fe25a7aa7f66c89f7bc12
Lorenzo L. Ancora <admin@fswdb.eu>
2023-09-01 18:08:55 +0200

    v. 010923.195

--- www/scripts/directory.js
+++ www/scripts/directory.js
@@ -13,26 +13,30 @@ $(function() {
     try {
         systemselector.val(navigator.platform.split(" ", 1)[0]);
     } catch {}
-    matchselector.val("pre");
+    matchselector.val("");
     matchfilter.val("");
     matchselector.on("change", function() {
         let selector = $(this);
         let label = $("#mlabel");
         switch (selector.val()) {
           case "pre":
-            mtype = "start";
+            mtype = "start with";
             break;
 
           case "pst":
-            mtype = "end";
+            mtype = "end with";
             break;
 
           case "ter":
-            mtype = "start and end";
+            mtype = "start and end with";
+            break;
+
+          case "snd":
+            mtype = "sound like";
             break;
 
           default:
-            mtype = "start";
+            mtype = "";
         }
     });
     function updfdesc() {
@@ -44,12 +48,16 @@ $(function() {
         } else {
             fdesc.text("");
         }
-        if (mtype != "" && mfval != "") {
-            fdesce.text(`Display only the resources that ${mtype} with ${mfval}.`);
-            submit.prop("disabled", false);
+        matchfilter.prop("disabled", mtype == "" || mtype == undefined);
+        if (mtype != "" && mtype != undefined && mfval != undefined && mfval != "") {
+            fdesce.text(`Display only the resources that ${mtype} “${mfval}”.`);
         } else {
             fdesce.text("");
+        }
+        if (tsval == "" || tsval === undefined || ssval == "" || ssval === undefined || mtype == "" || mtype === undefined || mfval == "" || mfval === undefined) {
             submit.prop("disabled", true);
+        } else {
+            submit.prop("disabled", false);
         }
     }
     function updfcategoryicon(e) {
@@ -62,9 +70,14 @@ $(function() {
         let osname = String($(e.target).val()).valueOf();
         if (osname !== undefined && osname != "") icon.attr("src", `/static/img/os/${osname}.webp`); else icon.attr("src", "/static/img/os/System.webp");
     }
+    function updffiltericon(e) {
+        let icon = $("img#filtericon");
+        let filtername = String($(e.target).val()).valueOf();
+        if (filtername !== undefined && filtername != "") icon.attr("src", `/static/img/filters/${filtername}.webp`); else icon.attr("src", "/static/img/filters/Filter.webp");
+    }
     $("form input, form select").on("change keyup touchend", updfdesc);
     taskselector.on("change", updfcategoryicon).trigger("change");
     systemselector.on("change", updfosicon).trigger("change");
-    matchselector.trigger("change");
+    matchselector.on("change", updffiltericon).trigger("change");
     updfdesc();
 });
